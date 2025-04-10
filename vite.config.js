b87badf07d5a9256ad1c4225583cff7d96b5edbe
Lee Teng Sean <2392944594@qq.com>
2025-04-10 16:45:58 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/fenchayuzhou/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/fenchayuzhou/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
