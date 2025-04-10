eb8b07af0f3cc8f274e005104b9a55b44b12a9be
Sean <2392944594@qq.com>
2025-04-10 21:09:04 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import { ref, reactive, computed, onMounted, watch } from "vue";
 import { initializeApp } from 'firebase/app'
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
-import defaultData from '@/assets/data.json';
 
 const firebaseApp = initializeApp({
   apiKey: 'AIzaSyA2RLK6hoiaJ7pdlNOXOnD_BwS62ABqC50',
@@ -13,15 +12,9 @@ const firebaseApp = initializeApp({
 const db = getFirestore(firebaseApp);
 const listRef = doc(db, 'fenchayuzhou', 'strategy');
 
-if (!localStorage.getItem('list'))
-  localStorage.setItem('list', JSON.stringify([defaultData]));
-const list = reactive(JSON.parse(localStorage.getItem('list')));
+const list = reactive([]);
 const url = 'https://hoyolabapi.vercel.app/api/fenchayuzhou?id=';
 
-// setDoc(listRef, {})
-//   .then(d => {
-//     console.log(d)
-//   })
 const base_type = {
   '1': '毁灭',
   '2': '巡猎',
@@ -32,6 +25,17 @@ const base_type = {
   '7': '丰饶',
   '8': '记忆',
 };
+
+const iconList = [
+  "https://act-webstatic.hoyoverse.com/darkmatter/hkrpg/prod_gf_cn/item_icon_u6975c/a33c526b158e79fb3fdb423d72fc9cae.png",
+  "https://act-webstatic.hoyoverse.com/darkmatter/hkrpg/prod_gf_cn/item_icon_u6975c/a1f70c6bb9713f81435821506865b4db.png",
+  "https://act-webstatic.hoyoverse.com/darkmatter/hkrpg/prod_gf_cn/item_icon_u6975c/3e9c8d9a74d5928012016f2bba0440e8.png",
+  "https://act-webstatic.hoyoverse.com/darkmatter/hkrpg/prod_gf_cn/item_icon_u6975c/d4ad4c5c30ec8b64cacb760563778bef.png",
+  "https://act-webstatic.hoyoverse.com/darkmatter/hkrpg/prod_gf_cn/item_icon_u6975c/15438b57925d433fdbe50a415567c156.png",
+  "https://act-webstatic.hoyoverse.com/darkmatter/hkrpg/prod_gf_cn/item_icon_u6975c/5059e2fb3d8b36ced88a00b84e58792c.png",
+  "https://act-webstatic.hoyoverse.com/darkmatter/hkrpg/prod_gf_cn/item_icon_u6975c/f70c86f524c7a5c6b930da71297844a6.png",
+  "https://act-webstatic.hoyoverse.com/darkmatter/hkrpg/prod_gf_cn/item_icon_u6975c/0bf8ad0e2732b28b3ce967e775eb23e7.png",
+];
 
 const button_list = [
   { name: 'formula', text: '推荐方程' },
@@ -47,6 +51,13 @@ function editColorString(text) {
   );
 }
 
+function preloadImages(urls) {
+  urls.forEach((url) => {
+    const img = new Image();
+    img.src = url;
+  });
+}
+
 async function onEnterUp(e) {
   if (e.keyCode == 13 && e.target.value !== '') {
     if (list.findIndex(d => d.id == e.target.value) == -1) {
@@ -54,7 +65,10 @@ async function onEnterUp(e) {
       const data = await response.json()
       e.target.value = ''
       list.push(data)
-      localStorage.setItem('list', JSON.stringify(list))
+      setDoc(listRef, { list })
+        .then(d => {
+          console.log(d)
+        })
     } else {
       alert(`ID: ${e.target.value} 已存在`)
       e.target.value = ''
@@ -62,53 +76,49 @@ async function onEnterUp(e) {
   }
 }
 
-const index = ref(0);
-const id = computed(() => index.value !== null ? list[index.value].id : null);
-const lineup = computed(() => index.value !== null ? list[index.value] : null);
-const info = computed(() => lineup.value !== null ? lineup.value.lineup_rogue_tourn_info : null);
-
-const title = computed(() => lineup.value !== null ? lineup.value.title : null);
-const description = computed(() => lineup.value !== null ? lineup.value.description : null);
-
-const formula_list_main = computed(() => info.value !== null ? info.value.formula_list : []);
-const formula_list_sub = computed(() => info.value !== null ? info.value.second_formula_list : []);
-
-const miracle_list_main = computed(() => info.value !== null ? info.value.miracle_list : []);
-const miracle_list_sub = computed(() => info.value !== null ? info.value.second_miracle_list : []);
-
-const avatar_list = computed(() => info.value !== null ? info.value.group_list[0].avatar_list : []);
-
-const buff_req = computed(() => {
-  let buffs = {}
-  for (const formula of [...formula_list_main.value, ...formula_list_sub.value]) {
-    const {
-      main_buff_type_id, main_buff_num, main_buff_small_icon,
-      sub_buff_type_id, sub_buff_num, sub_buff_small_icon
-    } = formula;
-    if (typeof buffs[main_buff_type_id] == 'undefined' || buffs[main_buff_type_id].num < main_buff_num)
-      buffs[main_buff_type_id] = { num: main_buff_num, icon: main_buff_small_icon };
-    if (typeof buffs[sub_buff_type_id] == 'undefined' || buffs[sub_buff_type_id] < sub_buff_num)
-      buffs[sub_buff_type_id] = { num: sub_buff_num, icon: sub_buff_small_icon };
+function getBuffReq(info) {
+  let buffs = {};
+  if (info) {
+    for (const formula of [...info.formula_list, ...info.second_formula_list]) {
+      const {
+        main_buff_type_id, main_buff_num, main_buff_small_icon,
+        sub_buff_type_id, sub_buff_num, sub_buff_small_icon
+      } = formula;
+      if (typeof buffs[main_buff_type_id] == 'undefined' || buffs[main_buff_type_id].num < main_buff_num)
+        buffs[main_buff_type_id] = { num: main_buff_num, icon: main_buff_small_icon };
+      if (typeof buffs[sub_buff_type_id] == 'undefined' || buffs[sub_buff_type_id] < sub_buff_num)
+        buffs[sub_buff_type_id] = { num: sub_buff_num, icon: sub_buff_small_icon };
+    }
   }
   return buffs;
+}
+
+const index = ref(null);
+
+const loaded_formula = reactive([]);
+const loaded_miracle = reactive([]);
+
+watch(index, () => {
+  if (list[index]) {
+    loaded_formula[index] = list[index].lineup_rogue_tourn_info.formula_list[0].name;
+    loaded_miracle[index] = list[index].lineup_rogue_tourn_info.miracle_list[0].name;
+    show.value = button_list[0].name;
+  }
 });
-const loaded_formula = ref(null);
-const loaded_miracle = ref(null);
-watch(id, () => {
-  loaded_formula.value = formula_list_main.value[0];
-  loaded_miracle.value = miracle_list_main.value[0];
-});
+
 onMounted(async () => {
-  loaded_formula.value = formula_list_main.value[0];
-  loaded_miracle.value = miracle_list_main.value[0];
+  preloadImages(iconList);
 
   const listData = (await getDoc(listRef)).data().list;
   for (const ld of listData) {
     if (list.findIndex(d => d.id == ld.id) == -1) {
       list.push(ld);
-      localStorage.setItem('list', JSON.stringify(list));
+      loaded_formula.push(ld.lineup_rogue_tourn_info.formula_list[0].name);
+      loaded_miracle.push(ld.lineup_rogue_tourn_info.miracle_list[0].name);
     }
   }
+  if (list.length > 0)
+    index.value = 0;
 });
 </script>
 
@@ -116,8 +126,7 @@ onMounted(async () => {
   <div class="container-fluid overflow-hidden">
     <div class="row">
       <div class="col-4 list">
-        <input class="form-control mb-2" type="text" placeholder="Input ID to get new Strategy Guide"
-          @keyup="onEnterUp($event)">
+        <input class="form-control mb-2" type="text" placeholder="输入ID来获取攻略" @keyup="onEnterUp($event)">
         <ol class="list-group list-group-numbered">
           <li class="list-group-item" :class="{ active: index == i }" v-for="[i, data] in Object.entries(list)"
             @click="index = i">
@@ -125,133 +134,139 @@ onMounted(async () => {
           </li>
         </ol>
       </div>
-      <div class="col-8 detail" v-if="id !== null">
-        <div class="card">
-          <div class="card-header">
-            <div class="card-text">
-              <small><small>ID: {{ id }}</small></small>
+      <template v-for="[i, { id, title, description, lineup_rogue_tourn_info: info }] in Object.entries(list)">
+        <div class="col-8 detail" v-show="index == i">
+          <div class="card">
+            <div class="card-header">
+              <div class="card-text">
+                <small><small>ID: {{ id }}</small></small>
+              </div>
+              <div class="card-title gold">
+                <span class="fs-3">{{ title }}</span>
+              </div>
             </div>
-            <div class="card-title gold">
-              <span class="fs-3">{{ title }}</span>
+            <div class="card-body">
+              <div class="card-text">
+                {{ description }}
+              </div>
             </div>
-          </div>
-          <div class="card-body">
-            <div class="card-text">
-              {{ description }}
-            </div>
-          </div>
-          <div class="card-footer">
-            <div class="row justify-content-center">
-              <div class="col-auto my-auto gold"><span class="fs-4">全方程需求</span></div>
-              <div class="col-auto" v-for="buff in buff_req">
-                <img :src="buff.icon" width="30">
-                <div class="text-center">{{ buff.num }}</div>
+            <div class="card-footer">
+              <div class="row justify-content-center">
+                <div class="col-auto my-auto gold"><span class="fs-4">全方程需求</span></div>
+                <div class="col-auto" v-for="buff in getBuffReq(info)">
+                  <img :src="buff.icon" width="30">
+                  <div class="text-center">{{ buff.num }}</div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="row justify-content-center mt-3">
-          <div class="col-auto">
-            <div class="btn-group" role="group" aria-label="Basic example">
-              <button type="button" class="btn btn-secondary" :class="{ active: show == button.name }"
-                v-for="button in button_list" @click="show = button.name">
-                {{ button.text }}
+          <div class="row justify-content-center mt-3">
+            <div class="col-auto">
+              <div class="btn-group" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-secondary" :class="{ active: show == button.name }"
+                  v-for="button in button_list" @click="show = button.name">
+                  {{ button.text }}
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="formula-box mt-4" v-show="show == 'formula'">
+            <div class="alert alert-info" v-if="info?.formula_description">
+              {{ info.formula_description }}
+            </div>
+            <h4 class="gold text-center">核心方程</h4>
+            <div class="btn-group w-100" role="group" aria-label="Basic example">
+              <button class="btn btn-outline-secondary" :class="{ active: loaded_formula[i] == formula.name }"
+                v-for="formula in info.formula_list" @click="loaded_formula[i] = formula.name">
+                <span class="gold">{{ formula.name }}</span>
               </button>
             </div>
-          </div>
-        </div>
-        <div class="formula-box mt-4" v-show="show == 'formula'">
-          <div class="alert alert-info" v-if="info.formula_description">
-            {{ info.formula_description }}
-          </div>
-          <h4 class="gold text-center">核心方程</h4>
-          <div class="btn-group w-100" role="group" aria-label="Basic example">
-            <button class="btn btn-outline-secondary" :class="{ active: loaded_formula == formula }"
-              v-for="formula in formula_list_main" @click="loaded_formula = formula">
-              <span class="gold">{{ formula.name }}</span>
-            </button>
-          </div>
-          <h4 class="gold mt-2 text-center">辅助方程</h4>
-          <div class="btn-group w-100" role="group" aria-label="Basic example">
-            <button class="btn btn-outline-secondary" :class="{ active: loaded_formula == formula }"
-              v-for="formula in formula_list_sub" @click="loaded_formula = formula">
-              <span class="gold">{{ formula.name }}</span>
-            </button>
-          </div>
-          <div class="mt-2" v-if="loaded_formula">
-            <div class="card">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-auto mt-auto">
-                    <h3 class="card-title gold">{{ loaded_formula.name }}</h3>
-                  </div>
-                  <div class="col-auto">
-                    <img :src="loaded_formula.main_buff_small_icon" width="30">
-                    <div class="text-center">{{ loaded_formula.main_buff_num }}</div>
-                  </div>
-                  <div class="col-auto">
-                    <img :src="loaded_formula.sub_buff_small_icon" width="30">
-                    <div class="text-center">{{ loaded_formula.sub_buff_num }}</div>
-                  </div>
-                </div>
-                <p class="card-text" v-html="editColorString(loaded_formula.desc)"></p>
-              </div>
+            <h4 class="gold mt-2 text-center" v-show="info.second_formula_list.length > 0">辅助方程</h4>
+            <div class="btn-group w-100" role="group" aria-label="Basic example">
+              <button class="btn btn-outline-secondary" :class="{ active: loaded_formula[i] == formula.name }"
+                v-for="formula in info.second_formula_list" @click="loaded_formula[i] = formula.name">
+                <span class="gold">{{ formula.name }}</span>
+              </button>
             </div>
-          </div>
-        </div>
-        <div class="row justify-content-center mt-4" v-show="show == 'avatar'">
-          <div class="col-auto">
-            <ul class="list-group list-group-horizontal">
-              <li class="list-group-item" v-for="avatar in avatar_list">
-                <div class="position-relative">
-                  <img :src="avatar.icon_url" width="80">
-                  <div class="base-type">{{ base_type[avatar.avatar_base_type] }}</div>
-                </div>
-                <div class="row" v-if="avatar.second_avatars.length > 0">
-                  <div class="col-auto mt-2">
-                    <template v-for="sub_avatar in avatar.second_avatars">
-                      <div class="sub_avatar" v-if="sub_avatar.item_name">
-                        <img :src="sub_avatar.icon_url">
+            <template v-for="formula in [...info.formula_list, ...info.second_formula_list]">
+              <div class="mt-2" v-show="formula.name == loaded_formula[i]">
+                <div class="card">
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col-auto mt-auto">
+                        <h3 class="card-title gold">{{ formula.name }}</h3>
                       </div>
-                    </template>
+                      <div class="col-auto">
+                        <img :src="formula.main_buff_small_icon" width="30">
+                        <div class="text-center">{{ formula.main_buff_num }}</div>
+                      </div>
+                      <div class="col-auto">
+                        <img :src="formula.sub_buff_small_icon" width="30">
+                        <div class="text-center">{{ formula.sub_buff_num }}</div>
+                      </div>
+                    </div>
+                    <p class="card-text" v-html="editColorString(formula.desc)"></p>
                   </div>
                 </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="row mt-4" v-show="show == 'miracle'" v-if="loaded_miracle">
-          <h4 class="gold text-center">核心加权奇物</h4>
-          <div class="btn-group w-100" role="group" aria-label="Basic example">
-            <button class="btn btn-outline-secondary" :class="{ active: loaded_miracle == miracle }"
-              v-for="miracle in miracle_list_main" @click="loaded_miracle = miracle">
-              <img :src="miracle.icon" height="40px">
-              <span class="gold px-3">{{ miracle.name }}</span>
-              <img :src="miracle.icon" height="40px">
-            </button>
-          </div>
-          <h4 class="gold mt-2 text-center">辅助加权奇物</h4>
-          <div class="btn-group w-100" role="group" aria-label="Basic example">
-            <button class="btn btn-outline-secondary" :class="{ active: loaded_miracle == miracle }"
-              v-for="miracle in miracle_list_sub" @click="loaded_miracle = miracle">
-              <img :src="miracle.icon" height="40px">
-              <span class="gold px-3">{{ miracle.name }}</span>
-              <img :src="miracle.icon" height="40px">
-            </button>
-          </div>
-          <div class="mt-2">
-            <div class="card">
-              <div class="card-body">
-                <h3 class="card-title gold">
-                  {{ loaded_miracle.name }}
-                  <img class="ms-3" :src="loaded_miracle.icon" height="40px">
-                </h3>
-                <p class="card-text" v-html="editColorString(loaded_miracle.desc)"></p>
               </div>
+            </template>
+          </div>
+          <div class="row justify-content-center mt-4" v-show="show == 'avatar'">
+            <div class="col-auto">
+              <ul class="list-group list-group-horizontal">
+                <li class="list-group-item" v-for="avatar in info.group_list[0].avatar_list">
+                  <div class="position-relative">
+                    <img :src="avatar.icon_url" width="80">
+                    <div class="base-type">{{ base_type[avatar.avatar_base_type] }}</div>
+                  </div>
+                  <div class="row" v-if="avatar.second_avatars.length > 0">
+                    <div class="col-auto mt-2">
+                      <template v-for="sub_avatar in avatar.second_avatars">
+                        <div class="sub_avatar" v-if="sub_avatar.item_name">
+                          <img :src="sub_avatar.icon_url">
+                        </div>
+                      </template>
+                    </div>
+                  </div>
+                </li>
+              </ul>
             </div>
           </div>
+          <div class="row mt-4" v-show="show == 'miracle'">
+            <h4 class="gold text-center">核心加权奇物</h4>
+            <div class="btn-group w-100" role="group" aria-label="Basic example">
+              <button class="btn btn-outline-secondary" :class="{ active: loaded_miracle[i] == miracle.name }"
+                v-for="miracle in info.miracle_list" @click="loaded_miracle[i] = miracle.name">
+                <img :src="miracle.icon" height="40px">
+                <span class="gold px-3">{{ miracle.name }}</span>
+                <img :src="miracle.icon" height="40px">
+              </button>
+            </div>
+            <h4 class="gold mt-2 text-center" v-show="info.second_miracle_list.length > 0">辅助加权奇物</h4>
+            <div class="btn-group w-100" role="group" aria-label="Basic example">
+              <button class="btn btn-outline-secondary" :class="{ active: loaded_miracle[i] == miracle.name }"
+                v-for="miracle in info.second_miracle_list" @click="loaded_miracle[i] = miracle.name">
+                <img :src="miracle.icon" height="40px">
+                <span class="gold px-3">{{ miracle.name }}</span>
+                <img :src="miracle.icon" height="40px">
+              </button>
+            </div>
+            <template v-for="miracle in [...info.miracle_list, ...info.second_miracle_list]">
+              <div class="mt-2" v-show="miracle.name == loaded_miracle[i]">
+                <div class="card">
+                  <div class="card-body">
+                    <h3 class="card-title gold">
+                      {{ miracle.name }}
+                      <img class="ms-3" :src="miracle.icon" height="40px">
+                    </h3>
+                    <p class="card-text" v-html="editColorString(miracle.desc)"></p>
+                  </div>
+                </div>
+              </div>
+            </template>
+          </div>
         </div>
-      </div>
+      </template>
     </div>
   </div>
 </template>
